e72af592061ccbadfce7d56e85386f5cb9185bc9
Naol <nlege001@plattsburgh.edu>
2025-04-06 17:03:57 -0400

    add create workout cta

--- app/src/main/java/com/example/getmefit/view/composables/ExerciseDetailsScreen.kt
+++ app/src/main/java/com/example/getmefit/view/composables/ExerciseDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.getmefit.view.composables
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,18 +15,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -63,34 +67,61 @@ fun ExerciseDetailsScreen(
 
     } else {
         val savedExercises = rememberSaveable { mutableStateOf<List<Exercise>>(emptyList()) }
-        Column {
-            if (version.hasAddRemoveOption) {
-                AddedExerciseCount(savedExercises.value.size)
-            }
+        Scaffold(
+            topBar = {
+                if (version.hasAddRemoveOption) {
+                    AddedExerciseCount(savedExercises.value.size)
+                }
+            },
+            content = { padding ->
+                Column(
+                    modifier = Modifier.padding(padding)
+                ) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        itemsIndexed(data) { index, item ->
+                            ExerciseCard(
+                                exercise = item,
+                                onClick = onClick,
+                                version = version,
+                                onRemove = {
+                                    savedExercises.value = savedExercises.value.minus(it)
+                                },
+                                onAdd = {
+                                    savedExercises.value = savedExercises.value.plus(it)
+                                },
+                                isAdded = { savedExercises.value.contains(it) },
+                            )
 
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                itemsIndexed(data) { index, item ->
-                    ExerciseCard(
-                        exercise = item,
-                        onClick = onClick,
-                        version = version,
-                        onRemove = {
-                            savedExercises.value = savedExercises.value.minus(it)
-                        },
-                        onAdd = {
-                            savedExercises.value = savedExercises.value.plus(it)
-                        },
-                        isAdded = { savedExercises.value.contains(it) },
-                    )
-
-                    if (index != data.lastIndex) {
-                        Spacer(Modifier.height(8.dp))
+                            if (index != data.lastIndex) {
+                                Spacer(Modifier.height(8.dp))
+                            }
+                        }
                     }
                 }
-            }
-        }
+            },
+            floatingActionButton = {
+                AnimatedVisibility(savedExercises.value.isNotEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(CircleShape)
+                            .background(Color.Black)
+                            .clickable(onClick = { }),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_add),
+                            contentDescription = "",
+                            tint = Color.White,
+                            modifier = Modifier.size(48.dp * 0.5f)
+                        )
+                    }
+                }
+            },
+        )
+
     }
 }
 
